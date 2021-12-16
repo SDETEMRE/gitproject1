c0162375ff9b6bbd58aaf73b95a8ae75333f0316
SDETEMRE <dincsoyemre@gmail.com>
2021-12-16 16:40:31 +0300

    few commits

--- src/work1/HelloClass.java
+++ src/work1/HelloClass.java
@@ -4,5 +4,6 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("work1 started");
         System.out.println("Created git repository");
+        System.out.println("made few commits");
     }
 }
